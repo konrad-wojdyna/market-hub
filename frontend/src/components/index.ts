916f22bec3a728364f897b076e3f24f4845d1978
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-02-05 09:39:55 +0100

    feat(frontend): add ListingCard and ListingList and shared components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ListingList } from "./listing/ListingList";
+export { default as ErrorComponent } from "./shared/ErrorComponent";
+export { default as Loading } from "./shared/LoadingComponent";
