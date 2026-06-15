ffbe8dc88da9065824a5367367f869fdc7631197
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-06-15 16:42:09 +0200

    feat: add senderId to MessageResponse

--- backend/src/main/java/com/markethub/api/dto/response/MessageResponse.java
+++ backend/src/main/java/com/markethub/api/dto/response/MessageResponse.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record MessageResponse(
       Long id,
       Long conversationId,
+      Long senderId,
       String firstName,
       String avatar,
       String content,
