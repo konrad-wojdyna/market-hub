ac46bd91a374717fdfa886c89c74d3bfe1008cd9
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-06-15 13:57:29 +0200

    feat: add Message entity

---
+++ backend/src/main/java/com/markethub/api/entity/Message.java
@@ -0,0 +1,41 @@
+package com.markethub.api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    private String content;
+
+    @Column(name = "is_read")
+    private boolean isRead;
+
+    @CreationTimestamp
+    @Column(name = "created_at",  nullable = false,  updatable = false)
+    private LocalDateTime createdAt;
+
+}
